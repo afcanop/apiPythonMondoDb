8ec52c9137c37cc7183c66e6fc278825b6fed569
andres <afcanop@gmail.com>
2022-10-14 11:09:28 -0500

    feat: se elimina codigo no necesario del api y

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from routes.logRouter import LogRouter
 
-app = FastAPI()
+app = FastAPI(
+    title="apiLogCromo",
+    description="Apis para el control del log de erp semantica"
+)
 
 app.include_router(LogRouter)
--- repository/logRepository.py
+++
@@ -1,8 +0,0 @@
-def userEntity(item) -> dict:
-    return {
-        "id": str(item["_id"]),
-        "data": dir
-    }
-
-def lista(entity) -> list:
-    return [item for item in entity]
